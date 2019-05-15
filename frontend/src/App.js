94f9d834f389f84f3337466412ce243fffb0ca79
brennedith <brennedithg@gmail.com>
2019-05-15 10:46:36 -0500

    Add base React auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Signup from './pages/Signup';
 import Login from './pages/Login';
 
 const App = () => {
   return (
     <Router>
       <Route exact path="/" component={Home} />
+      <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
     </Router>
   );
