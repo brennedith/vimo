4b2d277fcd8c2935e8e7d01d9f6f31b3c6f43ed0
brennedith <brennedithg@gmail.com>
2019-05-15 16:23:41 -0500

    1. Fix typo in deploy command 2. Add NODE_ENV based endpoints

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,5 +1,11 @@
+const production_endpoint = 'https://vimo-backend.herokuapp.com';
+const development_endpoint = 'http://localhost:5000';
+
 const config = {
-  endpoint: 'https://vimo-backend.herokuapp.com'
+  endpoint:
+    process.env.NODE_ENV === 'production'
+      ? production_endpoint
+      : development_endpoint
 };
 
 export default config;
