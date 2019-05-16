edb899efbffc8c15666ab5080b7704770e8013ac
brennedith <brennedithg@gmail.com>
2019-05-16 12:31:54 -0500

    Refactor NewPostForm link to MobileApp

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="title">Home</h1>
       <p>
         <Link to="/signup">/signup</Link>
         <br />
         <Link to="/login">/login</Link>
         <br />
-        <Link to="/post/new">/post/new</Link>
+        <Link to="/app">/app</Link>
       </p>
     </div>
   );
