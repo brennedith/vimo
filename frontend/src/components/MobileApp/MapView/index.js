56463e7c62d30d662f40af01a24c5439b3c617bf
brennedith <brennedithg@gmail.com>
2019-05-16 19:20:30 -0500

    Add missing files from MapView

---
+++ frontend/src/components/MobileApp/MapView/index.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
+
+import appContext from '../../../services/context';
+
+const MapView = () => {
+  const mapBoxToken = process.env.REACT_APP_MAPBOX_TOKEN;
+  const { state, dispatch } = useContext(appContext);
+  const { latitude, longitude } = state.coords;
+  const { zoom } = state.map;
+
+  const viewport = {
+    width: '100%',
+    height: '100%',
+    latitude,
+    longitude,
+    zoom
+  };
+
+  const updateZoom = zoom => {
+    dispatch({
+      type: 'UPDATE_ZOOM',
+      payload: zoom
+    });
+  };
+
+  if (!latitude || !longitude) return null;
+
+  return (
+    <div className="MapView" style={{ width: '100%', height: '100%' }}>
+      <ReactMapGL
+        mapboxApiAccessToken={mapBoxToken}
+        reuseMaps
+        {...viewport}
+        mapStyle={'mapbox://styles/mapbox/streets-v11'}
+        onViewportChange={({ zoom }) => updateZoom(zoom)}
+      >
+        <Marker latitude={latitude} longitude={longitude}>
+          <div>x</div>
+        </Marker>
+        <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
+          <NavigationControl
+            onViewportChange={({ zoom }) => updateZoom(zoom)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default MapView;
