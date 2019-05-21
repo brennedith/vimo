91bfa8e117e722defef77ce47df6122c82b68d3b
brennedith <brennedithg@gmail.com>
2019-05-21 15:06:10 -0500

    Add default avatar picture

--- frontend/src/pages/MobileApp/components/Profile/AvatarPicture.js
+++ frontend/src/pages/MobileApp/components/Profile/AvatarPicture.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './AvatarPicture.css';
 
-const AvatarPicture = ({ url }) => (
-  <figure
-    className="AvatarPicture"
-    style={{ backgroundImage: `url(${url})` }}
-  />
-);
+const AvatarPicture = ({ url }) => {
+  const pictureUrl = url ? url : '/assets/avatar.png';
+
+  return (
+    <figure
+      className="AvatarPicture"
+      style={{ backgroundImage: `url(${pictureUrl})` }}
+    />
+  );
+};
 
 export default AvatarPicture;
