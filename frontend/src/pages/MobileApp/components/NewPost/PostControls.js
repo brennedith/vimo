75d47af8ab705e571b19058ecdeb3f14cf5c7729
brennedith <brennedithg@gmail.com>
2019-05-22 17:39:22 -0500

    Refactor accuracy check, add speed check

--- frontend/src/pages/MobileApp/components/NewPost/PostControls.js
+++ frontend/src/pages/MobileApp/components/NewPost/PostControls.js
@@ -4,19 +4,21 @@ import appContext from '../../../../services/context';
 
 import './PostControls.css';
 
-const Controls = ({ children, handleSend, leftPanel, rightPanel }) => {
+const Controls = ({ handleSend, leftPanel, rightPanel }) => {
   const { state } = useContext(appContext);
   const { speed, accuracy } = state.coords;
 
-  /* TODO: Alert if accuracy is poor */
-  const accuracyClass = accuracy > 50 ? 'bad' : 'good';
+  /* TODO: Alert if accuracy is poor or speed too fast*/
+  const accuracyCheck = accuracy > 50;
+  const speedCheck = speed > 20;
+  const buttonStatusClass = accuracyCheck || speedCheck ? 'bad' : 'good';
 
   return (
     <div className="controls">
       {leftPanel ? leftPanel : <div className="push" />}
       <button
         type="button"
-        className={`accuracy-button ${accuracyClass}`}
+        className={`accuracy-button ${buttonStatusClass}`}
         onClick={handleSend}
       >
         Place
