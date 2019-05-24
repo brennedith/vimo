80ec5613ec0df0a9edd69d469307c809f21215e7
brennedith <brennedithg@gmail.com>
2019-05-24 14:22:29 -0500

    Fix mirror of posts media

--- frontend/src/pages/MobileApp/components/Feed/PostContent.js
+++ frontend/src/pages/MobileApp/components/Feed/PostContent.js
@@ -3,24 +3,26 @@ import React from 'react';
 import './PostContent.css';
 
 const PostContent = ({ content, closeContent }) => {
-  const { type } = content;
+  const { type, frontCamera } = content;
 
   setTimeout(() => {
     closeContent();
   }, 11000);
 
+  const frontCameraClass = frontCamera ? 'content mirror' : 'content';
+
   return (
     <article className="PostContent">
       {type === 'text' && (
-        <div className="content" style={content.style}>
+        <div className={frontCameraClass} style={content.style}>
           <span>{content.text}</span>
         </div>
       )}
       {type === 'photo' && (
-        <img className="content" src={content.mediaURL} alt="post" />
+        <img className={frontCameraClass} src={content.mediaURL} alt="post" />
       )}
       {type === 'video' && (
-        <video className="content" src={content.mediaURL} autoPlay controls />
+        <video className={frontCameraClass} src={content.mediaURL} autoPlay />
       )}
     </article>
   );
